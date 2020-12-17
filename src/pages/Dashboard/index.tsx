3bcd4466aa34c65c1a087f1b5df5ea0d4c8c838b
Piscinao <henhvj@gmail.com>
2020-12-16 21:22:12 -0300

    Adicionar e remover prato

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -40,7 +40,12 @@ const Dashboard: React.FC = () => {
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
     try {
-      // TODO ADD A NEW FOOD PLATE TO THE API
+      const response = await api.post('/foods', {
+        ...food,
+        available: true,
+      });
+
+      setFoods([...foods, response.data]);
     } catch (err) {
       console.log(err);
     }
@@ -53,7 +58,13 @@ const Dashboard: React.FC = () => {
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    try {
+      await api.delete(`/foods/${id}`);
+
+      setFoods(foods.filter(food => food.id !== id));
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   function toggleModal(): void {
