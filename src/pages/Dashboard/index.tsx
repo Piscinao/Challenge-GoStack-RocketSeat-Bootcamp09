91b851d1ba03e4874c4378f804e6706438e6edb7
Piscinao <henhvj@gmail.com>
2020-12-16 21:40:33 -0300

    Edicao de prato

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -54,6 +54,19 @@ const Dashboard: React.FC = () => {
   async function handleUpdateFood(
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
+    try {
+      const response = await api.put(`/foods/${editingFood.id}`, {
+        ...editingFood,
+        ...food,
+      });
+
+      setFoods(
+        foods.map(mappedFood => mappedFood.id === editingFood.id ? { ...response.data} : mappedFood, 
+          ),
+      );
+    } catch (err) {
+      console.log(err);
+    }
     // TODO UPDATE A FOOD PLATE ON THE API
   }
 
@@ -76,7 +89,8 @@ const Dashboard: React.FC = () => {
   }
 
   function handleEditFood(food: IFoodPlate): void {
-    // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    setEditingFood(food);
+    toggleEditModal();
   }
 
   return (
